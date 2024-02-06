4eaca3f6ad4a3fa27f6532f454be220de5fa9b23
An Tae Gyun <boltcarrier@naver.com>
2024-02-06 21:58:14 +0900

    Leetcode #344 문제풀이

---
+++ src/main/java/com/example/codingtest/leetcode/문자열/lc_344/Solution.java
@@ -0,0 +1,26 @@
+package com.example.codingtest.leetcode.문자열.lc_344;
+
+/**
+ * #344 문자열 뒤집기
+ * https://leetcode.com/problems/reverse-string/description/
+ */
+
+public class Solution {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+
+        // 서로 중앙으로 이동해 나가다 겹치는 지점에 도달하면 종료
+        while (start < end) {
+            // 임시 변수를 이용해 값 스왑
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+
+            // 앞쪽 문자는 한 칸 뒤로, 뒤쪽 문자는 한 칸 앞으로 이동
+            start++;
+            end--;
+        }
+    }
+
+}
