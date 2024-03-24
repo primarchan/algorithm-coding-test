a4df5fd2a303e60b11c96093c06d8f4a023d4d8d
An T.G <boltcarrier@naver.com>
2024-03-24 14:22:56 +0900

    Leetcode #328 번 [홀짝 연결 리스트 (Odd Even Linked List)] 문제 풀이

---
+++ src/main/java/com/example/codingtest/leetcode/연결리스트/ListNode.java
@@ -0,0 +1,18 @@
+package com.example.codingtest.leetcode.연결리스트;
+
+// Definition for singly-linked list.
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {}
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val; this.next = next;
+    }
+
+}
