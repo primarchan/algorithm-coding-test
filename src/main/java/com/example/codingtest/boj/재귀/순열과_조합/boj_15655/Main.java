a118a070bd46358035436a0f0e711985b9dc010d
An T.G <boltcarrier@naver.com>
2024-05-02 08:42:51 +0900

    BOJ #15655 번 [N과 M(6)] 문제 풀이

---
+++ src/main/java/com/example/codingtest/boj/재귀/순열과_조합/boj_15655/Main.java
@@ -0,0 +1,53 @@
+package com.example.codingtest.boj.재귀.순열과_조합.boj_15655;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static int n, m;
+    static int[] numbers;
+    static boolean[] check;
+    static int[] output;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        numbers = new int[n];
+        check = new boolean[n];
+        output = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        Arrays.sort(numbers);
+        perm(0, 0);
+    }
+
+    public static void perm(int depth, int start) {
+        // base case
+        if (depth == m) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < m; i++) {
+                sb.append(output[i]).append(" ");
+            }
+            System.out.println(sb);
+
+            return;
+        }
+
+        // recursive case
+        for (int i = start; i < n; i++) {
+            if (!check[i]) {
+                check[i] = true;
+                output[depth] = numbers[i];
+                perm(depth + 1, i + 1);
+                check[i] = false;
+            }
+        }
+    }
+
+}
