6ccfe6b1c27576567d3177479ad9aba9940b01d9
An T.G <boltcarrier@naver.com>
2024-06-10 22:07:52 +0900

    [코딩 테스트 합격자 되기] 문제 08번 풀이

---
+++ src/main/java/com/example/codingtest/sample/배열/문제_08/Solution.java
@@ -0,0 +1,37 @@
+package com.example.codingtest.sample.배열.문제_08;
+
+/**
+ * 올바른 괄호
+ * https://programmers.co.kr/learn/courses/30/lessons/12909
+ * https://github.com/retrogemHK/codingtest_java/blob/main/solution/08.java
+ *
+ * 제약 조건
+ * 문자열 s 의 길이 : 1000,000 이하의 자연수
+ * 문자열 s 는 '(' 또는 ')' 로만 이루어져 있습니다.
+ *
+ * 시간복잡도 : O(N)
+ */
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+    public boolean solution(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        char[] a = s.toCharArray();
+
+        for (char c : a) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty() || stack.pop() == c) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+}
