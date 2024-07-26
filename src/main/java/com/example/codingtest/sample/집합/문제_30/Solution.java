76ca5d6610d435dbb4b2946e70c57b38d6cefe61
An T.G <boltcarrier@naver.com>
2024-07-27 06:36:31 +0900

    [코딩 테스트 합격자 되기] 문제 30 번 풀이

---
+++ src/main/java/com/example/codingtest/sample/집합/문제_30/Solution.java
@@ -0,0 +1,11 @@
+package com.example.codingtest.sample.집합.문제_30;
+
+/**
+ * 길 찾기 게임 (⭐⭐️)
+ * https://github.com/retrogemHK/codingtest_java/blob/main/solution/30.java
+ *
+ * 시간복잡도 : O(NlogN)
+ */
+
+public class Solution {
+}
