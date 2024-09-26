a0670b6df397b40491a610c686396888cfd9a84d
An T.G <boltcarrier@naver.com>
2024-09-26 13:49:15 +0900

    BOJ #9466 번 [텀 프로젝트] 문제 풀이

---
+++ src/main/java/com/example/codingtest/boj/그래프/dfs/boj_9466/Main.java
@@ -0,0 +1,52 @@
+package com.example.codingtest.boj.그래프.dfs.boj_9466;
+
+import java.util.Scanner;
+
+public class Main {
+    static int[] nextNode;
+    static int[] depth;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+
+        for (int t = 0; t < tc ; t++) {
+            int n = sc.nextInt();
+            nextNode = new int[n + 1];
+            depth = new int[n + 1];
+
+            for (int i = 1; i <= n; i++) {
+                nextNode[i] = sc.nextInt();
+                depth[i] = 0;
+            }
+
+            int cnt = 0;
+            for ( int i = 1; i <= n ; i++){
+                if (depth[i] == 0) {
+                    depth[i] = 1;
+                    cnt += dfs(i);
+                }
+            }
+            System.out.println(n - cnt);
+        }
+    }
+
+    static int dfs(int nodeNum) {
+        int next = nextNode[nodeNum];
+        int cycleCnt = 0;
+
+        // 첫 방문
+        if (depth[next] == 0) {
+            depth[next] = depth[nodeNum] + 1;
+            cycleCnt = dfs(next);
+        } else { // 재방문(사이클)
+            cycleCnt = depth[nodeNum] - depth[next] + 1;
+        }
+
+        // 다음 탐색을 위해 재귀 안에서 초기화
+        depth[nodeNum] = 100_001;
+
+        return Math.max(cycleCnt, 0);
+    }
+
+}
